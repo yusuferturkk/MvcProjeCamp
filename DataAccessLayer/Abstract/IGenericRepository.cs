252aea6f6e59f7c343250dd7dcff2f5856096398
yusuferturkk <yusuf_erturk98@hotmail.com>
2023-03-16 15:56:58 +0300

    Add project files.

---
+++ DataAccessLayer/Abstract/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericRepository<TEntity> 
+        where TEntity : class, new()
+    {
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        TEntity GetById(int id);
+        List<TEntity> GetList();
+        List<TEntity> GetList(Expression<Func<TEntity, bool>> filter);
+    }
+}
---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,65 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity>
+        where TEntity : class, new()
+    {
+        public void Add(TEntity entity)
+        {
+            using (var context = new Context())
+            {
+                context.Set<TEntity>().Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (var context = new Context())
+            {
+                context.Set<TEntity>().Remove(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (var context = new Context())
+            {
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity GetById(int id)
+        {
+            using (var context = new Context())
+            {
+                return context.Set<TEntity>().Find(id);
+            }
+        }
+
+        public List<TEntity> GetList()
+        {
+            using (var context = new Context())
+            {
+                return context.Set<TEntity>().ToList();
+            }
+        }
+
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter)
+        {
+            using (var context = new Context())
+            {
+                return context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+    }
+}
